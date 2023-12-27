dd88a2cf5381d217a798b63c524537d9b5461723
Thibault Liotard <thibaultliotard0875@gmail.com>
2023-12-27 14:37:37 +0100

    Change to responsivity system on the main menu

--- app/main_menu.py
+++ app/main_menu.py
@@ -3,20 +3,27 @@ from app import player
 from app import inventory
 from app import equipment
 
-
 def run_main_menu():
     window = tk.Tk()
     window.title("Tool Main Menu")
-    window.minsize(800,600)
-    window.resizable = True
 
-    player_button = tk.Button(window, text="Player Page", command=lambda:player.open_player_page(window))
-    inventory_button = tk.Button(window, text="Inventory Object Page", command=lambda:inventory.open_inventory_page)
-    equipment_button = tk.Button(window, text="Equipment Parts Page", command=lambda:equipment.open_equipment_page)
+    # Définir la taille initiale et la taille minimale
+    window.geometry("800x600")
+    window.minsize(800, 600)  # Taille minimale de la fenêtre
 
+    window.resizable(True, True)  # Rendre la fenêtre redimensionnable
+
+    # Créer un cadre pour centrer les boutons
+    button_frame = tk.Frame(window)
+    button_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+    player_button = tk.Button(button_frame, text="Player Page", command=lambda: player.open_player_page(window))
+    inventory_button = tk.Button(button_frame, text="Inventory Object Page", command=lambda: inventory.open_inventory_page(window))
+    equipment_button = tk.Button(button_frame, text="Equipment Parts Page", command=lambda: equipment.open_equipment_page(window))
+
+    # Organiser les boutons dans le cadre
     player_button.pack(pady=10)
     inventory_button.pack(pady=10)
     equipment_button.pack(pady=10)
-
 
     window.mainloop()
